8254c9c897ce74268a0edfabe6a6ae9351a8a6ce
marschwarz <114850613+marschwarz@users.noreply.github.com>
2023-05-25 13:47:52 +0200

    Update recipe_repository.dart

--- lib/repositories/recipe_repository.dart
+++ lib/repositories/recipe_repository.dart
@@ -1,11 +1,10 @@
 import 'package:flutter/material.dart';
 import '../models/recipe.dart';
 
-abstract class RecipeRepository extends ChangeNotifier{
+abstract class RecipeRepository extends ChangeNotifier {
   Future<List<Recipe>> getAllRecipes();
-  Future<Recipe> getRecipe(String id);
+  Future<Recipe?> getRecipe(String id);
   Future<void> addRecipe(Recipe recipe);
   Future<void> updateRecipe(Recipe recipe);
   Future<void> deleteRecipe(String id);
-
 }
